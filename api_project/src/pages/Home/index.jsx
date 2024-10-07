cd675f935fba4a6db78b8a76bb8d6a26ec80c84f
Diogo Ferreira Pedebos <124224219+DiogoPedebos@users.noreply.github.com>
2024-10-07 20:08:48 -0300

    Mudando a lógica da aplicação para conter backend e front no mesmo projeto

---
+++ api_project/src/pages/Home/index.jsx
@@ -0,0 +1,69 @@
+import {useState, useRef, useEffect } from 'react'
+import './style.css'
+// import api from '../../services/api.js'
+// import Trash from '../../assets/img.ex' importação de imagem de exemplo
+
+function Home() {
+
+  let dados_day = []
+  const [count, setCount] = useState(0)
+  const inputCidade = useRef()
+  const inputLatitude = useRef()
+  const inputLongitude = useRef()
+
+  // async function getDados_day(){
+  //   const resposta = await api.get('/listar')
+  //   dados_day = await resposta.json()
+  // }
+
+  function ProcuraCidade(){
+    console.log(inputCidade.current.value)
+  }
+
+  function ProcuraCoordenada(){
+
+    console.log(inputLatitude.current.value)
+    console.log(inputLatitude.current.value)
+  }
+  
+
+  return (
+
+    <div className="container">
+      <div>
+        <div>
+            <h1>Seu guia para o tempo</h1>
+            {/* <button type='button'><img src={Trash}/></button> */}
+            <button onClick={ProcuraCidade}>Pesquisar1</button>
+            <input ref={inputCidade} type="text" placeholder="Nome da Cidade"/>
+        </div>
+        <div>
+            <button onClick={ProcuraCoordenada} type='button'>Pesquisar2</button>
+            <input ref={inputLatitude} name='lat' type="number"  placeholder="Latitude"/>
+            <input ref={inputLongitude} name='lon' type="number"  placeholder="Longitude"/>
+        </div>
+      </div>
+
+      {dados_day.map(dado_day => (
+
+        <div className="containerCentral" key={dado_day.id}>
+          <h1>vDiaSemana {dado_day.tempo}</h1>
+          <p>Temperatura Mínima: {dado_day.temperaturaMinima}</p>
+          <p>Temperatura Máxima: {dado_day.temperaturaMaxima}</p>
+          <p>Atual Clima: {dado_day.pictocode}</p>
+          <p>Velocidade do Vento: {dado_day.velocidadeDoVentoMedia}</p>
+          <p>Direção do Vento: {dado_day.direcaoDoVento}</p>
+          <p>Probabilidade de precipitação: {dado_day.probabilidadeDePrecipitacao} </p>
+        </div>
+
+      ))}
+
+    </div>
+
+
+
+      
+  )
+}
+
+export default Home
